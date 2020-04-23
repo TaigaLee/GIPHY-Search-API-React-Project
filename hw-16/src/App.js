6fa8c459f0020e2995c1678d690cf6a7c5f14a60
taigalee <taiga@taigalee.com>
2020-04-22 22:54:45 -0500

    search bar rendering

---
+++ hw-16/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hw-16/src/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
+  handleInputChange = event => {
+    let searchTerm = this.state.searchTerm;
+    searchTerm = event.target.value;
+    this.setState({
+      searchTerm: searchTerm
+    });
+  };
+
+  render() {
+    return (
+      <div className="SearchBar-div">
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleInputChange} />
+          <button>Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
